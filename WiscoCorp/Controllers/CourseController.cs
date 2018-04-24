421599aa83ac6408af68bd2b7e0bf8da143517f1
Nicolas Crespo <ncrespo@my.wctc.edu>
2018-04-23 19:00:19 -0500

    Add project files.

---
+++ WiscoCorp/Controllers/CourseController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WiscoCorp.Controllers
+{
+    public class CourseController : Controller
+    {
+        // GET: Course
+        public ActionResult Courses()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ WiscoCorp/Controllers/InstructorController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WiscoCorp.Controllers
+{
+    public class InstructorController : Controller
+    {
+        // GET: Instructor
+        public ActionResult Account()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
